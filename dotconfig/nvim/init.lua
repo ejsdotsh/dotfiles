2f0ce9ccb7a42eefc4759850680886042f622ce6
e.j. (they/them) <git@ejs.sh>
2023-09-28 07:56:58 -0700

    updated init.lua, using neovim vscode extension

--- dotconfig/nvim/init.lua
+++ dotconfig/nvim/init.lua
@@ -5,56 +5,63 @@
     /e.j.
 --]]
 
--- disable unused/unwanted builtin plugins
--- see ./lua/ejs/disable.lua
-require("ejs.disable")
+if vim.g.vscode then
+  -- VSCode extension
+else
+  -- ordinary Neovim
+  
+  -- disable unused/unwanted builtin plugins
+  -- see ./lua/ejs/disable.lua
+  require("ejs.disable")
 
--- globals that i expect to be setup
--- see ./lua/ejs/options.lua
-require("ejs.options")
+  -- globals that i expect to be setup
+  -- see ./lua/ejs/options.lua
+  require("ejs.options")
 
--- set leader to " "
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+  -- set leader to " "
+  vim.g.mapleader = " "
+  vim.g.maplocalleader = " "
 
--- apply additional/personal keymaps
--- ./lua/ejs/keymaps.lua
-require("ejs.keymaps")
+  -- apply additional/personal keymaps
+  -- ./lua/ejs/keymaps.lua
+  require("ejs.keymaps")
 
--- plugin manager - install packer
--- adapted from kickstart.nvim and from https://github.com/wbthomason/packer.nvim#bootstrapping
--- see ./lua/ejs/plugins.lua
-require("ejs.plugins")
+  -- plugin manager - install packer
+  -- adapted from kickstart.nvim and from https://github.com/wbthomason/packer.nvim#bootstrapping
+  -- see ./lua/ejs/plugins.lua
+  require("ejs.plugins")
 
--- mason and LSP configs
--- specific configurations sourced from ./lua/ejs/lsp/init.lua
-require("ejs.lsp.config")
+  -- mason and LSP configs
+  -- specific configurations sourced from ./lua/ejs/lsp/init.lua
+  require("ejs.lsp.config")
 
--- autocompletion
--- see ./lua/ejs/cmplete.lua
-require("ejs.cmplete")
+  -- autocompletion
+  -- see ./lua/ejs/cmplete.lua
+  require("ejs.cmplete")
 
--- nvim-tree
--- load the default configuration, but don't open automagically
-require("nvim-tree").setup()
+  -- nvim-tree
+  -- load the default configuration, but don't open automagically
+  require("nvim-tree").setup()
 
--- dap
-require("ejs.dap.config")
+  -- dap
+  require("ejs.dap.config")
 
--- treesitter
-require("ejs.treesit")
+  -- treesitter
+  require("ejs.treesit")
 
-local cmd = vim.api.nvim_command
-cmd "colorscheme catppuccin-macchiato"
+  local cmd = vim.api.nvim_command
+  cmd "colorscheme catppuccin-macchiato"
 
-require("nvim-web-devicons").setup{
-  color_icons = true;
-  default = true;
-}
+  require("nvim-web-devicons").setup{
+    color_icons = true;
+    default = true;
+  }
 
--- lualine
--- set the theme, everything else is default...for now
-require("lualine").setup{}
+  -- lualine
+  -- set the theme, everything else is default...for now
+  require("lualine").setup{}
+
+end
 
 -- modeline
 -- vim: ts=2 sts=2 sw=2 et
