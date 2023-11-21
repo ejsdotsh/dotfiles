8a4bce9fa1dc3235d4d8339218d3d3802d6aec18
e.j. (they/them) <git@ejs.sh>
2023-11-20 19:32:27 -0800

    lazy updates, setup script stuff

--- scripts/arch_setup.sh
+++ scripts/arch_setup.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
+# something like debugging
 set -x
-
-
-sudo pacman -S --noconfirm \
-  neovim neovim-plugins python-pynvim \
-  kitty kitty-terminfo
 
 # purge
 sudo pacman -Rsn --noconfirm \
-  nano nano-syntax-highlighting
+	nano nano-syntax-highlighting
+
+# ensure that some systems..ish stuff is installed
+sudo pacman -S \
+	openssh curl wget ripgrep fd lazygit \
+	iwd wireless_tools wpa_supplicant \
+	neovim neovim-plugins python-pynvim \
+	kitty kitty-terminfo
 
 # hyprland and wayland apps
-sudo pacman -S --noconfirm \
-  mako wofi \
-  hyprland xdg-desktop-portal-hyprland hyprpaper \
-  waybar \
-  swaylock swayidle \
-  qt5-wayland qt6-wayland \
-  pipewire wireplumber
+sudo pacman -S \
+	mako wofi \
+	hyprland xdg-desktop-portal-hyprland swaybg \
+	wl-clipboard waybar \
+	swaylock swayidle \
+	qt5-wayland qt6-wayland \
+	pipewire wireplumber
 
 # font and LaTeX installation
-sudo pacman -S --noconfirm \
-  tectonic \
-  ttf-dejavu-nerd \
-  ttf-firacode-nerd \
-  ttf-font-awesome \
-  ttf-inconsolata-nerd \
-  ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono \
-  ttf-ubuntu-nerd ttf-ubuntu-mono-nerd
+sudo pacman -S \
+	tectonic \
+	ttf-dejavu-nerd \
+	ttf-firacode-nerd \
+	ttf-font-awesome \
+	ttf-inconsolata-nerd \
+	ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono \
+	ttf-ubuntu-nerd ttf-ubuntu-mono-nerd
