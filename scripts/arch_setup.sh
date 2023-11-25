0331959235beed6517fbb05640937e2d7afabba6
e.j. (they/them) <git@ejs.sh>
2023-11-25 15:19:35 -0800

    some endeavour stuff

--- scripts/arch_setup.sh
+++ scripts/arch_setup.sh
@@ -18,7 +18,8 @@ sudo pacman -S --needed --noconfirm \
 	nnn atool libarchive zip unzip trash-cli sshfs rclone fuse2 \
 	iwd wireless_tools wpa_supplicant \
 	neovim neovim-plugins python-pynvim \
-	kitty kitty-terminfo tree yay apparmor
+	kitty kitty-terminfo tree yay apparmor \
+	mtr
 
 # hyprland and wayland apps
 sudo pacman -S --needed --noconfirm \
