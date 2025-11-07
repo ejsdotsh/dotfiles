b9a118c2b30faaa410b182544ecb9031d6fe7deb
e.j. sahala (they/them) <git@ejs.sh>
2025-11-06 17:28:19 -0800

    Dev (#8)
    * moving scripts
    
    * updates and stuff
    
    * set/fix xdg paths
    
    * taskwarrior config update
    
    * scripts work better when executable
    
    * river color update
    
    * update taskrc version; way-display settings for laptop
    
    * add typst, add alias for podman
    
    * styling updates, waybar, mako
    
    * river init, waybar update
    
    * starting to rewrite setup script
    
    * zathura updates
    
    * zathura theme
    
    * endeavour packages
    
    * helix and dprint updates
    
    * helix updates
    
    * helix language updates
    
    * adding nickel to helix, improving rust/cargo support
    
    * remove ignored files from index
    
    * helix languages and switch to niri
    
    * customizing niri
    
    * waybar and niri updates
    
    * typo in waybar config
    
    * add systemd stuff
    
    * helix, niri, waybar updates
    
    * add helper script, update helix
    
    * add dprint
    
    * don't format experimental stuff

--- scripts/endeavouros_setup.sh
+++ scripts/endeavouros_setup.sh
@@ -15,28 +15,28 @@ sudo pacman -Syu --noconfirm
 # ensure that some systems..ish stuff is installed
 sudo pacman -S --needed --noconfirm \
 	openssh curl wget ripgrep fd lazygit \
-	nnn atool libarchive zip unzip trash-cli sshfs rclone fuse2 \
+	atool libarchive zip unzip trash-cli sshfs rclone fuse2 \
 	iwd wireless_tools wpa_supplicant \
 	neovim neovim-plugins python-pynvim \
-	kitty kitty-terminfo tree yay apparmor \
-	mtr bluez-utils acpi network-manager-applet \
+	kitty kitty-terminfo tree apparmor \
+	mtr bluez-utils acpi paru network-manager-applet \
 	btop
 
 # hyprland and wayland apps
 sudo pacman -S --needed --noconfirm \
 	mako wofi polkit-gnome \
-	hyprland xdg-desktop-portal-hyprland \
 	xdg-utils xdg-desktop-portal-gtk \
 	polkit-kde-agent polkit-qt6 gnome-keyring \
-	swaybg swayidle \
+	swayidle \
 	wl-clipboard wayland-utils \
 	qt5-wayland qt6-wayland qt5ct qt6ct \
-	pipewire wireplumber \
-	grim slurp
+	pipewire wireplumber
+	# grim slurp
 
-yay -Syua --needed --noconfirm --sudoloop \
-	waybar-hyprland swaylock-effects nwg-look \
-	catppuccin-gtk-theme-mocha
+# TODO rewrite with paru
+# yay -Syua --needed --noconfirm --sudoloop \
+# 	waybar-hyprland swaylock-effects nwg-look \
+# 	catppuccin-gtk-theme-mocha
 #catppuccin-gtk-theme-macchiato catppuccin-gtk-theme-frappe catppuccin-gtk-theme-latte
 
 # font and LaTeX installation
@@ -54,16 +54,39 @@ sudo pacman -S --needed --noconfirm \
 	podman \
 	python-pip python-black python-poetry
 
-yay -S --needed --noconfirm --sudoloop \
-	opentofu
+# TODO rewrite with paru
+# yay -S --needed --noconfirm --sudoloop \
+# 	opentofu
 #podman-desktop \
 
-if [ ! -e "/usr/bin/terraform" ]; then
-	sudo ln -s /usr/bin/tofu /usr/bin/terraform
-fi
+# if [ ! -e "/usr/bin/terraform" ]; then
+# 	sudo ln -s /usr/bin/tofu /usr/bin/terraform
+# fi
 
+# TODO rewrite with paru
 # security stuff
-yay -S --needed --noconfirm --sudoloop \
-	1password 1password-cli
+# yay -S --needed --noconfirm --sudoloop \
+# 	1password 1password-cli
+
+# systemd for mako, waybar, and swayidle
+function user_systemd() {
+	systemctl --user add-wants niri.service mako.service
+	systemctl --user add-wants niri.service waybar.service
+
+	cat > $DTFS/dotconfig/systemd/user/swayidle.service << EOF
+[Unit]
+PartOf=graphical-session.target
+After=graphical-session.target
+Requisite=graphical-session.target
+
+[Service]
+ExecStart=/usr/bin/swayidle -w timeout 601 'niri msg action power-off-monitors' timeout 600 'swaylock -f' before-sleep 'swaylock -f'
+Restart=on-failure
+EOF
+
+	systemctl --user add-wants niri.service swayidle.service
+	# reload systemd
+	systemctl --user daemon-reload
+}
 
 exit 0
